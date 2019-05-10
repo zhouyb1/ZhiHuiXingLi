db438fcd1f804813c5b6418b9585494af9bda267
龚斌 <1304520+billgong@users.noreply.gitee.com>
2019-05-10 15:57:10 +0800

    no commit message

---
+++ source/PC/Ayma.Application.WebApi/Modules/ErpApi/SmallProgramClientApi.cs
@@ -0,0 +1,36 @@
+﻿using Nancy;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Ayma.Application.WebApi.Modules.ErpApi
+{
+    public class SmallProgramClientApi
+        : PdaBaseApi
+    {
+        public SmallProgramClientApi()
+            : base("/pdaapi")
+        {
+            Post["/SubmitBillSaleOut"] = SubmitBillSaleOut; //提交车班补货单
+        }
+
+        /// <summary>
+        /// 提交车班补货单
+        /// </summary>
+        /// <returns></returns>
+        public Response SubmitBillSaleOut(dynamic _)
+        {
+            int errCode = 0;
+            string errText = "";
+            if (errCode == 100)
+            {
+                return Success(errText);
+            }
+            else
+            {
+                return Fail(errText);
+            }
+        }
+    }
+}
---
+++ source/PC/Ayma.Application.WebApi/Modules/ErpApi/SmallProgramServerApi.cs
@@ -0,0 +1,35 @@
+﻿using Nancy;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Ayma.Application.WebApi.Modules.ErpApi
+{
+    public class SmallProgramServerApi : PdaBaseApi
+    {
+        public SmallProgramServerApi()
+            : base("/pdaapi")
+        {
+            Post["/SubmitBillSaleOut"] = SubmitBillSaleOut; //提交车班补货单
+        }
+
+        /// <summary>
+        /// 提交车班补货单
+        /// </summary>
+        /// <returns></returns>
+        public Response SubmitBillSaleOut(dynamic _)
+        {
+            int errCode = 0;
+            string errText = "";
+            if (errCode == 100)
+            {
+                return Success(errText);
+            }
+            else
+            {
+                return Fail(errText);
+            }
+        }
+    }
+}
