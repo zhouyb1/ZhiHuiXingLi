ea272e45ac7c4a5a5a1af76948832b80a1f4f9d7
张寄聪 <4823026+ZJiCong@users.noreply.gitee.com>
2019-05-14 10:37:42 +0800

    no commit message

---
+++ source/PC/Ayma.Framework.Module/Ayma.Application.Module/Ayma.Application.Mapping/TwoDev/T_OrderLogisticsInfoMap.cs
@@ -0,0 +1,27 @@
+﻿using Ayma.Application.TwoDevelopment.TwoDev;
+using System.Data.Entity.ModelConfiguration;
+
+namespace  Ayma.Application.Mapping
+{
+    /// <summary>
+    /// 创 建：超级管理员
+    /// 日 期：2019-05-14 10:37
+    /// 描 述：订单物流信息
+    /// </summary>
+    public partial class T_OrderLogisticsInfoMap : EntityTypeConfiguration<T_OrderLogisticsInfoEntity>
+    {
+        public T_OrderLogisticsInfoMap()
+        {
+            #region 表、主键
+            //表
+            this.ToTable("T_ORDERLOGISTICSINFO");
+            //主键
+            this.HasKey(t => t.F_Id);
+            #endregion
+
+            #region 配置关系
+            #endregion
+        }
+    }
+}
+
---
+++ source/PC/Ayma.Framework.Module/Ayma.Application.Module/Ayma.Application.TwoDevelopment/TwoDev/OrderLogistics/T_OrderLogisticsInfoEntity.cs
@@ -0,0 +1,72 @@
+﻿using Ayma.Util;
+using System;
+using System.ComponentModel.DataAnnotations.Schema;
+namespace Ayma.Application.TwoDevelopment.TwoDev
+
+{
+    /// <summary>
+    /// 创 建：超级管理员
+    /// 日 期：2019-05-14 10:37
+    /// 描 述：订单物流信息
+    /// </summary>
+    public partial class T_OrderLogisticsInfoEntity 
+    {
+        #region 实体成员
+        /// <summary>
+        /// 标示
+        /// </summary>
+        /// <returns></returns>
+        [Column("F_ID")]
+        public string F_Id { get; set; }
+        /// <summary>
+        /// 订单号
+        /// </summary>
+        /// <returns></returns>
+        [Column("F_ORDERNO")]
+        public string F_OrderNo { get; set; }
+        /// <summary>
+        /// 状态描述
+        /// </summary>
+        /// <returns></returns>
+        [Column("F_STATEDESCRIBE")]
+        public string F_StateDescribe { get; set; }
+        /// <summary>
+        /// 操作时间
+        /// </summary>
+        /// <returns></returns>
+        [Column("F_STATEDATETIME")]
+        public DateTime? F_StateDateTime { get; set; }
+        /// <summary>
+        /// 操作人
+        /// </summary>
+        /// <returns></returns>
+        [Column("F_STATEOPERATOR")]
+        public string F_StateOperator { get; set; }
+        /// <summary>
+        /// 对客户开放  1、开放；0、不开放
+        /// </summary>
+        /// <returns></returns>
+        [Column("F_CUSTOMEROPEN")]
+        public string F_CustomerOpen { get; set; }
+        #endregion
+
+        #region 扩展操作
+        /// <summary>
+        /// 新增调用
+        /// </summary>
+        public void Create()
+        {
+            this.F_Id = Guid.NewGuid().ToString();
+        }
+        /// <summary>
+        /// 编辑调用
+        /// </summary>
+        /// <param name="keyValue"></param>
+        public void Modify(string keyValue)
+        {
+            this.F_Id = keyValue;
+        }
+        #endregion
+    }
+}
+
