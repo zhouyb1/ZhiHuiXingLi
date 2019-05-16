8a8221c44ddffeb6a0c20ebae7f2a68c49c27c19
张寄聪 <4823026+ZJiCong@users.noreply.gitee.com>
2019-05-16 18:57:57 +0800

    no commit message

---
+++ source/PC/Ayma.Framework.Module/Ayma.Application.Module/Ayma.Application.Mapping/TwoDev/T_OrderCollectMoneyMap.cs
@@ -0,0 +1,27 @@
+﻿using Ayma.Application.TwoDevelopment.TwoDev;
+using System.Data.Entity.ModelConfiguration;
+
+namespace  Ayma.Application.Mapping
+{
+    /// <summary>
+    /// 创 建：超级管理员
+    /// 日 期：2019-05-16 10:59
+    /// 描 述：收款表
+    /// </summary>
+    public partial class T_OrderCollectMoneyMap : EntityTypeConfiguration<T_OrderCollectMoneyEntity>
+    {
+        public T_OrderCollectMoneyMap()
+        {
+            #region 表、主键
+            //表
+            this.ToTable("T_ORDERCOLLECTMONEY");
+            //主键
+            this.HasKey(t => t.F_Id);
+            #endregion
+
+            #region 配置关系
+            #endregion
+        }
+    }
+}
+
---
+++ source/PC/Ayma.Framework.Module/Ayma.Application.Module/Ayma.Application.Mapping/TwoDev/T_OrderPayMoneyMap.cs
@@ -0,0 +1,27 @@
+﻿using Ayma.Application.TwoDevelopment.TwoDev;
+using System.Data.Entity.ModelConfiguration;
+
+namespace  Ayma.Application.Mapping
+{
+    /// <summary>
+    /// 创 建：超级管理员
+    /// 日 期：2019-05-16 10:59
+    /// 描 述：付款表
+    /// </summary>
+    public partial class T_OrderPayMoneyMap : EntityTypeConfiguration<T_OrderPayMoneyEntity>
+    {
+        public T_OrderPayMoneyMap()
+        {
+            #region 表、主键
+            //表
+            this.ToTable("T_ORDERPAYMONEY");
+            //主键
+            this.HasKey(t => t.F_Id);
+            #endregion
+
+            #region 配置关系
+            #endregion
+        }
+    }
+}
+
