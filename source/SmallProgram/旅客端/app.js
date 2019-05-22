c1f31a4eb0685429866164be5dca33ed7122ebf6
胡说霸道的英雄 <1242510+aijiumai@users.noreply.gitee.com>
2019-05-22 17:23:07 +0800

    增加强制更新小程序

--- source/SmallProgram/旅客端/app.js
+++ source/SmallProgram/旅客端/app.js
@@ -51,7 +51,35 @@ App({
           })
         }
       }
-    })
+    });
+
+    // 更新
+    const updateManager = wx.getUpdateManager()
+    updateManager.onCheckForUpdate(function(res) {
+      // 请求完新版本信息的回调
+      if (res.hasUpdate) {
+        updateManager.onUpdateReady(function() {
+          wx.showModal({
+            title: '更新提示',
+            content: '新版本已经准备好，是否重启应用？',
+            success: function(res) {
+              if (res.confirm) {
+                // 新的版本已经下载好，调用 applyUpdate 应用新版本并重启
+                updateManager.applyUpdate()
+              }
+            }
+          })
+        });
+      }
+    });
+    updateManager.onUpdateFailed(function() {
+      // 新的版本下载失败
+      wx.showModal({
+        title: '更新提示',
+        content: '新版本下载失败',
+        showCancel: false
+      })
+    });
   },
   globalData: {
     userInfo: null
@@ -66,22 +94,5 @@ App({
     if (obj === 3) {
       return "1";
     };
-  },
-  get_time() {
-    var date = new Date();
-    var seperator1 = "-";
-    var seperator2 = ":";
-    var month = date.getMonth() + 1;
-    var strDate = date.getDate();
-    if (month >= 1 && month <= 9) {
-      month = "0" + month;
-    }
-    if (strDate >= 0 && strDate <= 9) {
-      strDate = "0" + strDate;
-    }
-    var currentdate = date.getFullYear() + seperator1 + month + seperator1 + strDate +
-      " " + date.getHours() + seperator2 + date.getMinutes() +
-      seperator2 + date.getSeconds();
-    return currentdate;
   }
 })
