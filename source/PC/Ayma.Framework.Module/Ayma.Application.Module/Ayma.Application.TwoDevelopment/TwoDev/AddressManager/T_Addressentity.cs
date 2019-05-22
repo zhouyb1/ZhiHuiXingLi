146aa17c2b1a8a2d25ad0e849085c60ef2d772a1
张寄聪 <4823026+ZJiCong@users.noreply.gitee.com>
2019-05-22 10:47:48 +0800

    no commit message

--- source/PC/Ayma.Framework.Module/Ayma.Application.Module/Ayma.Application.TwoDevelopment/TwoDev/AddressManager/T_Addressentity.cs
+++ source/PC/Ayma.Framework.Module/Ayma.Application.Module/Ayma.Application.TwoDevelopment/TwoDev/AddressManager/T_Addressentity.cs
@@ -9,12 +9,31 @@ namespace Ayma.Application.TwoDevelopment.TwoDev.AddressManager
 {
     public class T_Addressentity
     {
+        /// <summary>
+        /// 标示
+        /// </summary>
         [Column("F_ID")]
         public string F_Id { get; set; }
+        /// <summary>
+        /// 地址
+        /// </summary>
         [Column("F_ADDRESS")]
         public string F_Address { get; set; }
+       /// <summary>
+       /// 微信OpenId
+       /// </summary>
         [Column("F_OPENID")]
         public string F_OpenId { get; set; }
+        /// <summary>
+        /// 姓名
+        /// </summary>
+        [Column("F_NAME")]
+        public string F_Name { get; set; }
+        /// <summary>
+        /// 电话
+        /// </summary>
+        [Column("F_PHONE")]
+        public string F_Phone { get; set; }
     }
     
 }
