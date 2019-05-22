ea2886394a5c79305372f04fca178b2d5ebebfef
张寄聪 <4823026+ZJiCong@users.noreply.gitee.com>
2019-05-22 14:39:04 +0800

    no commit message

--- source/PC/Ayma.Framework.Module/Ayma.Application.Module/Ayma.Application.TwoDevelopment/TwoDev/OrderInquiry/T_OrderBodyEntity.cs
+++ source/PC/Ayma.Framework.Module/Ayma.Application.Module/Ayma.Application.TwoDevelopment/TwoDev/OrderInquiry/T_OrderBodyEntity.cs
@@ -72,5 +72,61 @@ namespace Ayma.Application.TwoDevelopment.TwoDev
         }
         #endregion
     }
+
+    public partial class T_GetBodyNameEntity
+    {
+        #region 实体成员
+        /// <summary>
+        /// 标示
+        /// </summary>
+        [Column("F_ID")]
+        public string F_Id { get; set; }
+        /// <summary>
+        /// 订单号
+        /// </summary>
+        [Column("F_ORDERNO")]
+        public string F_OrderNo { get; set; }
+        /// <summary>
+        /// 托运单号
+        /// </summary>
+        [Column("F_CONSIGNMENTNUMBER")]
+        public string F_ConsignmentNumber { get; set; }
+        /// <summary>
+        /// 重量 KG，默认1 
+        /// </summary>
+        [Column("F_WEIGHT")]
+        public decimal? F_Weight { get; set; }
+        /// <summary>
+        /// 配送距离 KM
+        /// </summary>
+        [Column("F_DISTANCE")]
+        public decimal? F_Distance { get; set; }
+        /// <summary>
+        /// 价格 元
+        /// </summary>
+        [Column("F_PRICE")]
+        public decimal? F_Price { get; set; }
+        /// <summary>
+        /// 数量 默认1件
+        /// </summary>
+        [Column("F_QTY")]
+        public int? F_Qty { get; set; }
+        /// <summary>
+        /// 订单状态
+        /// </summary>
+        [Column("FB_STATE")]
+        public string FB_State { get; set; }
+        /// <summary>
+        /// 分拣员
+        /// </summary>
+        [Column("F_NAME")]
+        public string F_Name { get; set; }
+        /// <summary>
+        /// 分拣员电话
+        /// </summary>
+        [Column("F_PHONE")]
+        public string F_Phone { get; set; }
+        #endregion
+    }
 }
 
