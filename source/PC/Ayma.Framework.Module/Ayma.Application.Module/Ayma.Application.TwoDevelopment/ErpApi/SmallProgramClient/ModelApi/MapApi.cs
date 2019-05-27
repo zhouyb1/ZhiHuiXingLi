1b2bb47df4688862e90c129c2796977c11552ae4
李启兵 <1317086+liqibing@users.noreply.gitee.com>
2019-05-27 10:48:52 +0800

    no commit message

---
+++ source/PC/Ayma.Framework.Module/Ayma.Application.Module/Ayma.Application.TwoDevelopment/ErpApi/SmallProgramClient/ModelApi/MapApi.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ayma.Application.TwoDevelopment.ErpApi.SmallProgramClient.ModelApi
+{
+
+    public class From
+    {
+        /// <summary>
+        /// 
+        /// </summary>
+        public double lat { get; set; }
+        /// <summary>
+        /// 
+        /// </summary>
+        public double lng { get; set; }
+    }
+
+    public class To
+    {
+        /// <summary>
+        /// 
+        /// </summary>
+        public double lat { get; set; }
+        /// <summary>
+        /// 
+        /// </summary>
+        public double lng { get; set; }
+    }
+
+    public class ElementsItem
+    {
+        /// <summary>
+        /// 
+        /// </summary>
+        public From from { get; set; }
+        /// <summary>
+        /// 
+        /// </summary>
+        public To to { get; set; }
+        /// <summary>
+        /// 
+        /// </summary>
+        public int distance { get; set; }
+        /// <summary>
+        /// 
+        /// </summary>
+        public int duration { get; set; }
+    }
+
+    public class Result
+    {
+        /// <summary>
+        /// 
+        /// </summary>
+        public List<ElementsItem> elements { get; set; }
+    }
+
+    public class Root
+    {
+        /// <summary>
+        /// 
+        /// </summary>
+        public int status { get; set; }
+        /// <summary>
+        /// 
+        /// </summary>
+        public string message { get; set; }
+        /// <summary>
+        /// 
+        /// </summary>
+        public Result result { get; set; }
+    }
+}
