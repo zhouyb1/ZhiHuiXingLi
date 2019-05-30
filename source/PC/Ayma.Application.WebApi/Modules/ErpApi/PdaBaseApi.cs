a02c8e450b137921a7e1ad4da4e4b9f06de19706
李启兵 <1317086+liqibing@users.noreply.gitee.com>
2019-05-30 15:32:16 +0800

    no commit message

--- source/PC/Ayma.Application.WebApi/Modules/ErpApi/PdaBaseApi.cs
+++ source/PC/Ayma.Application.WebApi/Modules/ErpApi/PdaBaseApi.cs
@@ -354,6 +354,12 @@ namespace Ayma.Application.WebApi.Modules.ErpApi
             ResParameter res = new ResParameter { code = ResponseCode.fail, info = info, data = new object { } };
             return Response.AsText(res.ToJson()).WithContentType("application/json");
         }
+        public Response FailString(string info, object data)
+        {
+            ResParameter res = new ResParameter { code = ResponseCode.fail, info = info, data = data };
+            return Response.AsText(res.ToJson()).WithContentType("application/json");
+        }
+
         /// <summary>
         /// 响应给微信服务器
         /// </summary>
