57ae3c35bbe17c6747e7851d12241455da2af7a7
李启兵 <190554503@qq.com>
2019-06-14 15:52:55 +0800

    no commit message

--- source/PC/Ayma.Application.WebApi/Modules/ErpApi/SmallProgramClientApi.cs
+++ source/PC/Ayma.Application.WebApi/Modules/ErpApi/SmallProgramClientApi.cs
@@ -17,6 +17,10 @@ using Config = Ayma.Util.Config;
 using Ayma.Application.Base.SystemModule;
 using Ayma.Application.TwoDevelopment.ErpApi.SmallProgramClient.ModelApi;
 using System.Data;
+using Aliyun.Acs.Core.Profile;
+using Aliyun.Acs.Core;
+using Aliyun.Acs.Dysmsapi.Model.V20170525;
+using Aliyun.Acs.Core.Exceptions;
 
 namespace Ayma.Application.WebApi.Modules.ErpApi
 {
@@ -29,7 +33,7 @@ namespace Ayma.Application.WebApi.Modules.ErpApi
         private Hashtable Parameters;
         private static string openId = string.Empty;
         private static string cellPhone = string.Empty;
-        private OrderInquiryIBLL order = new OrderInquiryBLL();
+        private static OrderInquiryIBLL order = new OrderInquiryBLL();
         private T_OpinionFeedbackIBLL feedbackIbll = new T_OpinionFeedbackBLL();
      
         public SmallProgramClientApi()
@@ -61,6 +65,7 @@ namespace Ayma.Application.WebApi.Modules.ErpApi
             Post["/GetPhone"] = GetPhone;
             Post["/SaveFeedBack"] = SaveFeedBack;
             Get["/testFee"] = testFee;
+            Get["/SendMessage"] = SendMessage;
         }
         private SmallProgramClientApiBLL billClientApiBLL = new SmallProgramClientApiBLL();
         private AirportMessageBLL airportService = new AirportMessageBLL();
@@ -906,6 +911,56 @@ namespace Ayma.Application.WebApi.Modules.ErpApi
                 Logger.Error("微信支付回调异常：" + ex.Message);
                 throw;
             }
+        }
+
+        public Response SendMessage(dynamic _)
+        {
+            var orderNo = this.GetReqData().ToJObject()["orderNo"].ToString();
+            sendSms(orderNo);
+            return Fail("");
+        }
+
+        //产品名称:云通信短信API产品,开发者无需替换
+        const String product = "Dysmsapi";
+        //产品域名,开发者无需替换
+        const String domain = "dysmsapi.aliyuncs.com";
+
+        // TODO 此处需要替换成开发者自己的AK(在阿里云访问控制台寻找)
+        const String accessKeyId = "LTAIr0X34gXbCQKE";
+        const String accessKeySecret = "UME00z7PeW0FLGFuRGAohBB5bc1A7t";
+        public SendSmsResponse sendSms(string orderNo)
+        {
+            IClientProfile profile = DefaultProfile.GetProfile("cn-hangzhou", accessKeyId, accessKeySecret);
+            DefaultProfile.AddEndpoint("cn-hangzhou", "cn-hangzhou", product, domain);
+            IAcsClient acsClient = new DefaultAcsClient(profile);
+            SendSmsRequest request = new SendSmsRequest();
+            SendSmsResponse response = null;
+            try
+            {
+                var orderData = order.GetT_OrderHeadEntity(orderNo);  //获取订单信息
+
+                //必填:待发送手机号。支持以逗号分隔的形式进行批量调用，批量上限为1000个手机号码,批量调用相对于单条调用及时性稍有延迟,验证码类型的短信推荐使用单条调用的方式
+                request.PhoneNumbers = orderData.F_FarePhone;
+                //必填:短信签名-可在短信控制台中找到
+                request.SignName = "智慧行李";
+                //必填:短信模板-可在短信控制台中找到
+                request.TemplateCode = "SMS_167971935";
+                //可选:模板中的变量替换JSON串,如模板内容为"亲爱的${name},您的验证码为${code}"时,此处的值为
+                request.TemplateParam = "{\"customer\":\"123\"}";
+                //可选:outId为提供给业务方扩展字段,最终在短信回执消息中将此值带回给调用者
+                request.OutId = "yourOutId";
+                //请求失败这里会抛ClientException异常
+                response = acsClient.GetAcsResponse(request);
+            }
+            catch (ServerException e)
+            {
+                Console.WriteLine(e.ErrorCode);
+            }
+            catch (ClientException e)
+            {
+                Console.WriteLine(e.ErrorCode);
+            }
+            return response;
         }
 
         /// <summary>
