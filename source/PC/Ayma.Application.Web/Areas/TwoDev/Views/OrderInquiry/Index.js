98963a1b43a2149880adf121c63e4b8ab934624f
李启兵 <190554503@qq.com>
2019-06-19 16:26:48 +0800

    no commit message

--- source/PC/Ayma.Application.Web/Areas/TwoDev/Views/OrderInquiry/Index.js
+++ source/PC/Ayma.Application.Web/Areas/TwoDev/Views/OrderInquiry/Index.js
@@ -142,19 +142,23 @@ var bootstrap = function ($, ayma) {
                                 cellvalue = "已取消";
                             } else if (cellvalue == -3) {
                                 colorcss = "label label-warning";
-                                cellvalue = "申请退款";
+                                cellvalue = "退款中";
                             }
                             else if (cellvalue == -2) {
                                 colorcss = "label label-warning";
                                 cellvalue = "已退款";
                             }
-                            else if (cellvalue == 2) {
+                            else if (cellvalue == 1) {
                                 colorcss = "label label-success";
                                 cellvalue = "未分拣";
                             }
-                            else if (cellvalue == 3) {
+                            else if (cellvalue == 2) {
                                 colorcss = "label label-success";
                                 cellvalue = "分拣中";
+                            }
+                            else if (cellvalue == 3) {
+                                colorcss = "label label-success";
+                                cellvalue = "分拣完成";
                             }
                             else if (cellvalue == 4) {
                                 colorcss = "label label-success";
@@ -175,19 +179,19 @@ var bootstrap = function ($, ayma) {
                             return "<span class='" + colorcss + "'>" + cellvalue + "</span>";
                         }
                     },
-                    { label: "航站楼", name: "F_AirfieldFloor", width: 160, align: "left" },
-                    { label: "航空公司", name: "F_FlightCompany", width: 160, align: "left" },
-                    { label: "航班号", name: "F_FlightNumber", width: 160, align: "left" },
-                    { label: "订单日期", name: "F_OrderDate", width: 160, align: "left" },
-                    { label: "订单号", name: "F_OrderNo", width: 160, align: "left" },
-                    { label: "客户姓名", name: "F_CustomerName", width: 160, align: "left" },
-                    { label: "联系电话", name: "F_CustomerPhone", width: 160, align: "left" },
+                    { label: "航站楼", name: "F_AirfieldFloor", width: 160, align: "center" },
+                    { label: "航空公司", name: "F_FlightCompany", width: 160, align: "center" },
+                    { label: "航班号", name: "F_FlightNumber", width: 160, align: "center" },
+                    { label: "订单日期", name: "F_OrderDate", width: 160, align: "center" },
+                    { label: "订单号", name: "F_OrderNo", width: 160, align: "center" },
+                    { label: "客户姓名", name: "F_CustomerName", width: 160, align: "center" },
+                    { label: "联系电话", name: "F_CustomerPhone", width: 160, align: "center" },
                     { label: "客户地址", name: "F_CustomerAddress", width: 220, align: "left" },
-                    { label: "客户备注", name: "F_CustomerRemarks", width: 160, align: "left" },
-                    { label: "订单创建类型", name: "F_CreateStype", width: 160, align: "left" },
-                    { label: "订单类型", name: "F_Stype", width: 160, align: "left" },
-                    { label: "订单创建日期", name: "F_CreateTime", width: 160, align: "left" },
-                    { label: "订单创建人", name: "F_CreateUserName", width: 160, align: "left" },
+                    { label: "客户备注", name: "F_CustomerRemarks", width: 160, align: "center" },
+                    { label: "订单创建类型", name: "F_CreateStype", width: 160, align: "center" },
+                    { label: "订单类型", name: "F_Stype", width: 160, align: "center" },
+                    { label: "订单创建日期", name: "F_CreateTime", width: 160, align: "center" },
+                    { label: "订单创建人", name: "F_CreateUserName", width: 160, align: "center" },
                 ],
                 mainId: 'F_Id',
                 reloadSelected: true,
