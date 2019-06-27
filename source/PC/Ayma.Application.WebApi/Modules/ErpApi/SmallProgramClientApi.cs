4b418d7c336be8798bab38989ba8ab6f139d0333
李启兵 <190554503@qq.com>
2019-06-27 15:29:13 +0800

    no commit message

--- source/PC/Ayma.Application.WebApi/Modules/ErpApi/SmallProgramClientApi.cs
+++ source/PC/Ayma.Application.WebApi/Modules/ErpApi/SmallProgramClientApi.cs
@@ -893,7 +893,6 @@ namespace Ayma.Application.WebApi.Modules.ErpApi
             }
         }
 
-
         /// <summary>
         /// 发送短信通知
         /// </summary>
@@ -918,19 +917,21 @@ namespace Ayma.Application.WebApi.Modules.ErpApi
             try
             {
                 var orderData = order.GetT_OrderHeadEntity(orderNo);  //获取订单信息
-
-                //必填:待发送手机号。支持以逗号分隔的形式进行批量调用，批量上限为1000个手机号码,批量调用相对于单条调用及时性稍有延迟,验证码类型的短信推荐使用单条调用的方式
-                request.PhoneNumbers = orderData.F_FarePhone;
-                //必填:短信签名-可在短信控制台中找到
-                request.SignName = "智慧行李";
-                //必填:短信模板-可在短信控制台中找到
-                request.TemplateCode = "SMS_167971935";
-                //可选:模板中的变量替换JSON串,如模板内容为"亲爱的${name},您的订单为${order}"时,此处的值为
-                request.TemplateParam = "{\"name\":\"" + orderData.F_FareName + "\",\"order\":\"" + orderData.F_OrderNo + "\"}";
-                //可选:outId为提供给业务方扩展字段,最终在短信回执消息中将此值带回给调用者
-                request.OutId = "yourOutId";
-                //请求失败这里会抛ClientException异常
-                response = acsClient.GetAcsResponse(request);
+                if (orderData != null)
+                {
+                    //必填:待发送手机号。支持以逗号分隔的形式进行批量调用，批量上限为1000个手机号码,批量调用相对于单条调用及时性稍有延迟,验证码类型的短信推荐使用单条调用的方式
+                    request.PhoneNumbers = orderData.F_FarePhone;
+                    //必填:短信签名-可在短信控制台中找到
+                    request.SignName = "智慧行李";
+                    //必填:短信模板-可在短信控制台中找到
+                    request.TemplateCode = "SMS_167971935";
+                    //可选:模板中的变量替换JSON串,如模板内容为"亲爱的${name},您的订单为${order}"时,此处的值为
+                    request.TemplateParam = "{\"name\":\"" + orderData.F_FareName + "\",\"order\":\"" + orderData.F_OrderNo + "\"}";
+                    //可选:outId为提供给业务方扩展字段,最终在短信回执消息中将此值带回给调用者
+                    request.OutId = "yourOutId";
+                    //请求失败这里会抛ClientException异常
+                    response = acsClient.GetAcsResponse(request);
+                }
             }
             catch (ServerException e)
             {
