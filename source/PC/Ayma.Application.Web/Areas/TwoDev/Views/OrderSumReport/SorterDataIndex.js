743a373b96de7e08eed793ce9e776dd4fa2997ec
李启兵 <190554503@qq.com>
2019-07-04 17:11:04 +0800

    no commit message

--- source/PC/Ayma.Application.Web/Areas/TwoDev/Views/OrderSumReport/SorterDataIndex.js
+++ source/PC/Ayma.Application.Web/Areas/TwoDev/Views/OrderSumReport/SorterDataIndex.js
@@ -26,13 +26,13 @@ var bootstrap = function ($, ayma) {
             $('#girdtable').jfGrid({
                 url: top.$.rootUrl + '/TwoDev/OrderSumReport/GetSorterReport',
                 headData: [
-                    { label: "分拣员姓名", name: "FJYName", width: 120, align: "left" },
-                    { label: "接单数量", name: "JDNum", width: 100, align: "left" },
-                    { label: "行李数量", name: "XLNum", width: 100, align: "left" },
-                    { label: "异常行李数", name: "YCXLNum", width: 100, align: "left" },
-                    { label: "被投诉数", name: "", width: 160, align: "left" },
+                    { label: "分拣员姓名", name: "FJYName", width: 120, align: "center" },
+                    { label: "接单数量", name: "JDNum", width: 100, align: "center" },
+                    { label: "行李数量", name: "XLNum", width: 100, align: "center" },
+                    { label: "异常行李数", name: "YCXLNum", width: 100, align: "center" },
+                    { label: "被投诉数", name: "", width: 160, align: "center" },
                     {
-                        label: "ATA到已分拣平均时间", name: "YFJTime", width: 160, align: "left",
+                        label: "ATA到已分拣平均时间", name: "YFJTime", width: 160, align: "center",
                         formatter: function (cellvalue, options, rowObject) {
                             var h = parseInt(cellvalue / 60);
                             var m = cellvalue % 60;
