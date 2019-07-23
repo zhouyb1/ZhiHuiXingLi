c3aae3e455ed48b11638eb138e7c4a18e39ed161
李启兵 <190554503@qq.com>
2019-07-23 16:57:33 +0800

    no commit message

---
+++ source/PC/Ayma.Framework.Module/Ayma.Application.Module/Ayma.Application.Mapping/TwoDev/T_OrderRefundMap.cs
@@ -0,0 +1,27 @@
+﻿using Ayma.Application.TwoDevelopment.TwoDev;
+using System.Data.Entity.ModelConfiguration;
+
+namespace  Ayma.Application.Mapping
+{
+    /// <summary>
+    /// 创 建：超级管理员
+    /// 日 期：2019-07-22 11:11
+    /// 描 述：微信申请退款
+    /// </summary>
+    public partial class T_OrderRefundMap : EntityTypeConfiguration<T_OrderRefundEntity>
+    {
+        public T_OrderRefundMap()
+        {
+            #region 表、主键
+            //表
+            this.ToTable("T_ORDERREFUND");
+            //主键
+            this.HasKey(t => t.F_Id);
+            #endregion
+
+            #region 配置关系
+            #endregion
+        }
+    }
+}
+
